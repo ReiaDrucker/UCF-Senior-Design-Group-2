c2007d5032da2c2287fbc53b3e498117d13cc47d
Reia Drucker <reia.drucker@gmail.com>
2021-10-11 15:17:41 -0400

    changed variable names to make more sense

--- GUI Visual Studio Project/point.py
+++ GUI Visual Studio Project/point.py
@@ -5,17 +5,17 @@ class Point(object):
 
     def __init__(self, name, x, y, z, length, width):
         self.name = name
-        self.realVector = np.array([x,y,z])
-        self.pixelVector = np.array([length, width])
+        self.realCoordinates = np.array([x,y,z])
+        self.pixelCoordinates = np.array([length, width])
 
     def __str__(self):
         return "Point {0}:\nReal Coordinates: {1}\t Pixel Coordinates: {2}".format(self.name, self.getRealCoordinates(), self.getPixelCoordinates())
 
     def getRealCoordinates(self):
-        return self.realVector
+        return self.realCoordinates
 
     def getPixelCoordinates(self):
-        return self.pixelVector
+        return self.pixelCoordinates
 
 
 
--- GUI Visual Studio Project/vector.py
+++ GUI Visual Studio Project/vector.py
@@ -6,17 +6,17 @@ class Vector(object):
     # This initialization gives a value equivalent to translating p1 to the origin and then translating p2 by the same amount
     def __init__(self, p1, p2):
         self.name = p1.name + p2.name
-        self.realVector = np.subtract(p2.getRealCoordinates(),p1.getRealCoordinates())
-        self.pixelVector = np.subtract(p2.getPixelCoordinates(),p1.getPixelCoordinates())
+        self.realCoordinates = np.subtract(p2.getRealCoordinates(),p1.getRealCoordinates())
+        self.pixelCoordinates = np.subtract(p2.getPixelCoordinates(),p1.getPixelCoordinates())
 
     def __str__(self):
         return "Point {0}:\nReal Coordinates: {1}\t Pixel Coordinates: {2}".format(self.name, self.getRealCoordinates(), self.getPixelCoordinates())
 
     def getRealCoordinates(self):
-        return self.realVector
+        return self.realCoordinates
 
     def getPixelCoordinates(self):
-        return self.pixelVector
+        return self.pixelCoordinates
 
 
 
