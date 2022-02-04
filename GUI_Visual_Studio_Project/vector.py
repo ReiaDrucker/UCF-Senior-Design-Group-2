434302830200825087c8c193da44c254a4532597
Reia Drucker <reia.drucker@gmail.com>
2022-02-04 14:37:25 -0500

    Some comments regarding error correction and project cleanup

--- GUI_Visual_Studio_Project/vector.py
+++ GUI_Visual_Studio_Project/vector.py
@@ -39,15 +39,25 @@ class Vector(DataTableRow):
         self['D'] = QtWidgets.QPushButton('Delete')
         self['D'].clicked.connect(self.delete)
 
+        # Grab a pointer to the depth Provider when we initialize
+        # This is technically a waste of memory but it works and is easy and probably won't be an issue
+        self.depthProviderRef = s.depthProvider;
+
     # Calculates percent of magnitude change and re-scales other vectors.
     def changeMag(self):
         if (self.prevDist != 0):
             percent = (self.dist/self.prevDist) * 100
-            print("Percentage:", percent,"%")
 
-            # Probably best if we grab the depth provider pointer from a point the vector was made with and make the changes through that
-            # Need to enable some sort of testing metric for before and after we do that with the tsukaba dataset
-            #updateDepthMap(percent)
+            # Issue here being that while depth is being calculated all the points update to their intermediary values maybe don't connect until that is done
+            # Shouldn't be able to plot anything until after that is done anyways so probabbly not a big deal, make sure everything is disabled until depth is calculated
+            # Can't test or run anything until this is setup 
+
+            #print("Percentage:", percent,"%")
+            #print(self.depthProviderRef.depth)
+            # Need to enable some sort of testing metric for before and after we do the error correction with the tsukaba dataset
+
+            # This function needs to be in the depth provider
+            #self.depthProviderRef.updateDepthMap(percent)
 
         self.prevDist = self.dist
 
