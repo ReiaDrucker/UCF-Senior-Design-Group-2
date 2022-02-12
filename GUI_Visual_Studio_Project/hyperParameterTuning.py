420aff80f03ad0eb12ad36c560e898e97b289476
Reia Drucker <reia.drucker@gmail.com>
2022-02-11 22:26:51 -0500

    GPU enabled + checkpointing for hp tuning

--- GUI_Visual_Studio_Project/hyperParameterTuning.py
+++ GUI_Visual_Studio_Project/hyperParameterTuning.py
@@ -1,20 +1,18 @@
 import numpy as np
 from ray import tune
 
+# Loss function we are seeking to minimize
+def objective(step, alpha, beta, charlie):
+    return alpha + beta + charlie
 
-# This is the loss function we are seeking to minimize
-def objective(step, alpha, beta):
-    return alpha + beta
-
-
-def training_function(config, checkpoint_dir=None):
+def training_function(config, checkpoint_dir="./hpTuning"):
     # Hyperparameters
-    alpha, beta = config["alpha"], config["beta"]
+    alpha, beta, charlie = config["alpha"], config["beta"], config["charlie"]
 
     # How many samples do we want for each pair
     for step in range(10):
         # Iterative training function - can be any arbitrary training procedure.
-        intermediate_score = objective(step, alpha, beta)
+        intermediate_score = objective(step, alpha, beta, charlie)
         # Feed the score back back to Tune.
         tune.report(mean_loss=intermediate_score)
 
@@ -22,18 +20,29 @@ def training_function(config, checkpoint_dir=None):
 def main(): 
 
     analysis = tune.run(
-    
+
         training_function,
+        name = "experiment",
+        local_dir = "./hpTuning",
+        verbose = 2,
+
+        # How much of your cpu and gpu to give each trial
+        # My machine has 16 threads so if passed 16 it would give one trial all 16 threads
+        # Here I am giving each trial no cpu cores and a hundreth of my gpu
+        resources_per_trial = {"cpu": 0, "gpu": 0.01},
 
         config =
         {
             # There are other options for how we want to distribute our options
             "alpha": tune.grid_search([0.001, 0.01, 0.1]),
-            "beta": tune.grid_search([1, 2, 3])
+            "beta": tune.grid_search([1, 2, 3]),
+            "charlie": tune.grid_search([-1, 1, 0])
         },
 
-        # This is specific to my desktop not sure what behavior you will get
-        resources_per_trial={"cpu": 16, "gpu": 2})
+        # Set to True to continue from a checkpoint if stopped
+        # Set to False if a new expirement is intended
+        resume = False
+        )
 
     print("Best config: ", analysis.get_best_config(metric="mean_loss", mode="min"))
     
