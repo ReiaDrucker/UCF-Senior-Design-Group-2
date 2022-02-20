Fix a few bugs, adjust scaling calculation

depth2/test2.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/bin/env python3.9
 
 import cv2 as cv
 import open3d as o3d
@@ -12,19 +12,20 @@ rgb = cv.imread('rgb.png', cv.IMREAD_GRAYSCALE)
 plt.imshow(disp)
 plt.show()
 
-def display_cloud(rgb, disparity, focal_length, baseline):
-    w, h = rgb.shape[:2]
+def display_cloud(rgb, disparity, focal_length, eps = 1e-3):
+    h, w = rgb.shape[:2]
 
-    effective_focal_length = baseline * focal_length
-    disparity = np.ones(disparity.shape) * disparity.max() - disparity
-    depth = (np.ones(disparity.shape) / disparity) * effective_focal_length
-    depth = depth.clip(0, 255).astype(np.uint8)
+    disparity = np.ones(disparity.shape) * disparity.max() * 2 - disparity
+
+    depth = np.ones(disparity.shape) / disparity
+
+    # plt.hist(depth.flatten(), bins=20)
+    # plt.show()
+
+    depth = (depth * 127 / depth.max()).astype(np.uint8)
 
     # depth[rgb == 0] = 255
     # rgb[rgb != 0] = 127
-
-    plt.hist(depth.flatten(), bins=20)
-    plt.show()
 
     rgb = o3d.geometry.Image(rgb)
     depth = o3d.geometry.Image(depth)
@@ -37,16 +38,18 @@ def display_cloud(rgb, disparity, focal_length, baseline):
     plt.imshow(rgbd.depth)
     plt.show()
 
-    guess = o3d.camera.PinholeCameraIntrinsic(w, h, focal_length, focal_length, w / 2, h / 2)
+    guess = o3d.camera.PinholeCameraIntrinsic(w, h, focal_length, focal_length, w / 1.8, h / 2)
     print(guess.intrinsic_matrix)
-
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, guess, project_valid_depth_only=False)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, guess, project_valid_depth_only=True)
     pcd.remove_non_finite_points()
     pcd, _ = pcd.remove_statistical_outlier(nb_neighbors = 20,
                                             std_ratio = 2.0)
+    # pcd.estimate_normals(
+    #    search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
     print(pcd)
 
     o3d.visualization.draw_geometries([pcd])
+    o3d.io.write_point_cloud("stereo.pcd", pcd)
 
-display_cloud(rgb, disp, 150, 1)
+display_cloud(rgb, disp, 305)